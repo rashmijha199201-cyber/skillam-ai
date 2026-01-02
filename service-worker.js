4f9d14692b639b304acf1e0b393bf515a0f9f6a3
rashmijha199201-cyber <rashmijha199201@gmail.com>
2026-01-03 00:57:57 +0530

    Implement service worker with caching strategies

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", e=>{
+ e.waitUntil(
+  caches.open("skillam-ai").then(cache=>{
+   return cache.addAll(["index.html","manifest.json"]);
+  })
+ );
+});
+
+self.addEventListener("fetch", e=>{
+ e.respondWith(
+  caches.match(e.request).then(res=>{
+   return res || fetch(e.request);
+  })
+ );
+});
